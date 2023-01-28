f268527a6f24642369ec254f68ec249600b6d791
Peter Zhechev <petar_vn7@abv.bg>
2023-01-29 00:15:17 +0200

    Update guessANumber.js
    Added limitation on attempts

--- guessANumber.js
+++ guessANumber.js
@@ -6,19 +6,36 @@ function game(input) {
     });
 
     let randomizer = Math.floor(Math.random() * 100);
+    let count = 5;
 
     let recursive = function () {
         readline.question('Guess the number (0 - 100): ', input => {
-           
+
             if (Number(input) <= 100 && Number(input) >= 0) {
                 if (Number(input) === randomizer) {
                     console.log('You guess it!');
                     recursive();
                 } else if (Number(input) < randomizer) {
                     console.log('Too Low!');
+                    count--;
+                    if (count > 1) {
+                        console.log(`You have ${count} more tries`);
+                    } else if (count === 1) {
+                        console.log(`You have ${count} more try`);
+                    } else if (count === 0) {
+                        console.log(`You lost the game\nGAME OVER`);
+                    }
                     recursive();
                 } else if (Number(input) > randomizer) {
                     console.log('Too High!');
+                    count--;
+                    if (count > 1) {
+                        console.log(`You have ${count} more tries`);
+                    } else if (count === 1) {
+                        console.log(`You have ${count} more try`);
+                    } else if (count === 0) {
+                        console.log(`You lost the game\nGAME OVER`);
+                    }
                     recursive();
                 }
             } else {
